231b2b4605aff1a80fb6adf1b2ef5b6cf3049795
Diyor11 <diyorq3arshi@gmail.com>
2022-03-03 20:37:57 +0500

    finish

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client'
 
 // const url = 'https://git.heroku.com/qdsocketserver'
-// const url = 'http://localhost:8090'
 const url = 'https://stormy-atoll-90492.herokuapp.com/'
 
 const socket = io(url)
